45cf8053c1890dd9c7bd26ef33b129810cbef3c1
Kyle Aranas <kebaranas@gmail.com>
2021-04-11 07:38:27 -0700

    object temperature sent using bluetooth low energy

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,15 @@
 #define HEALTH_THERMOMETER_SERVICE_UUID BLEUUID((uint16_t) 0x1809)
 // Identify the characteristic as a temperature measurement.
 #define TEMPERATURE_MEASUREMENT_CHARACTERISTIC_UUID BLEUUID((uint16_t) 0x2A1C)
+// Value calibration for the contactless IR sensor.
+#define CALIBRATION_VALUE 0 
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 BLEServer *server;
 BLEService *tempService;
 BLECharacteristic *objTempCelChar;
 
+int val = 0;
 void setup() {
   // Initiate serial monitor.
   Serial.begin(9600);
@@ -25,21 +28,22 @@ void setup() {
   BLEDevice::init("TracingCzar-Proto");;
   server = BLEDevice::createServer();
   tempService = server->createService(HEALTH_THERMOMETER_SERVICE_UUID);
-  objTempCelChar = tempService->createCharacteristic(TEMPERATURE_MEASUREMENT_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+  objTempCelChar = tempService->createCharacteristic(TEMPERATURE_MEASUREMENT_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_INDICATE);
   tempService->start();
   
+  // Set Bluetooth advertising settings.
   BLEAdvertising *advertising = BLEDevice::getAdvertising();
   advertising->addServiceUUID(HEALTH_THERMOMETER_SERVICE_UUID);
   advertising->setScanResponse(true);
   // advertising->setMinPreferred(0x06); // iOS
   advertising->setMinPreferred(0x12); // Android
-  // Start advertising.
   BLEDevice::startAdvertising();
 }
 
 void loop() {
   double temp = mlx.readObjectTempC();
   objTempCelChar->setValue(temp);
-  objTempCelChar->notify();
-  delay(1000);
+  objTempCelChar->indicate();
+  Serial.println(temp);
+  delay(5000);
 }
